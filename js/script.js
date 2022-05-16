0b1465d61e9334a32b532ef9cababe7d38fa80a7
WendyChi <wendy.jeremy.chi@gmail.com>
2022-05-17 00:01:58 +0700

    update test

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 function initMap() {
-  // The location of markers
-  // var restObj = '';
-
+  // Ajax request
   var xhr = new XMLHttpRequest();
 
   xhr.onload = function () {
@@ -23,66 +21,264 @@ function initMap() {
   xhr.open("get", "js/place.json");
   xhr.send();
 
-  // The Map
+  // The Map create Object
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 15,
     center: { lat: 1.28692, lng: 103.85457 },
+    styles: [
+      {
+        featureType: "all",
+        elementType: "all",
+        stylers: [
+          {
+            saturation: "-100",
+          },
+        ],
+      },
+      {
+        featureType: "administrative",
+        elementType: "all",
+        stylers: [
+          {
+            saturation: "-100",
+          },
+        ],
+      },
+      {
+        featureType: "landscape",
+        elementType: "all",
+        stylers: [
+          {
+            saturation: "-100",
+          },
+        ],
+      },
+      {
+        featureType: "landscape.man_made",
+        elementType: "all",
+        stylers: [
+          {
+            visibility: "on",
+          },
+          {
+            saturation: "-100",
+          },
+          {
+            gamma: "1",
+          },
+        ],
+      },
+      {
+        featureType: "poi",
+        elementType: "all",
+        stylers: [
+          {
+            saturation: "-100",
+          },
+          {
+            visibility: "simplified",
+          },
+        ],
+      },
+      {
+        featureType: "poi",
+        elementType: "geometry.fill",
+        stylers: [
+          {
+            saturation: "-100",
+          },
+        ],
+      },
+      {
+        featureType: "poi",
+        elementType: "labels",
+        stylers: [
+          {
+            visibility: "simplified",
+          },
+        ],
+      },
+      {
+        featureType: "poi",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            saturation: "-100",
+          },
+          {
+            weight: "6.61",
+          },
+          {
+            lightness: "0",
+          },
+          {
+            gamma: "1.5",
+          },
+        ],
+      },
+      {
+        featureType: "poi",
+        elementType: "labels.icon",
+        stylers: [
+          {
+            saturation: "-100",
+          },
+          {
+            gamma: "1.5",
+          },
+          {
+            weight: "0.01",
+          },
+        ],
+      },
+      {
+        featureType: "poi.park",
+        elementType: "geometry.fill",
+        stylers: [
+          {
+            color: "#e3e3e3",
+          },
+        ],
+      },
+      {
+        featureType: "road",
+        elementType: "labels.icon",
+        stylers: [
+          {
+            saturation: "-100",
+          },
+        ],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "all",
+        stylers: [
+          {
+            saturation: "-100",
+          },
+        ],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry.fill",
+        stylers: [
+          {
+            saturation: "-3",
+          },
+          {
+            color: "#e2e2e2",
+          },
+        ],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry.stroke",
+        stylers: [
+          {
+            visibility: "off",
+          },
+        ],
+      },
+      {
+        featureType: "transit",
+        elementType: "all",
+        stylers: [
+          {
+            saturation: "-100",
+          },
+        ],
+      },
+      {
+        featureType: "transit",
+        elementType: "labels.text",
+        stylers: [
+          {
+            saturation: "-100",
+          },
+        ],
+      },
+      {
+        featureType: "transit",
+        elementType: "labels.icon",
+        stylers: [
+          {
+            saturation: "-100",
+          },
+        ],
+      },
+      {
+        featureType: "water",
+        elementType: "all",
+        stylers: [
+          {
+            saturation: "-100",
+          },
+        ],
+      },
+    ],
   });
 
+  // Function to add Marker
   function addMarker(props) {
     const latLng = new google.maps.LatLng(props.latitude, props.longitude);
-
-    const image = "img/About.png";
 
     const marker = new google.maps.Marker({
       position: latLng,
       map: map,
-      label: props.name,
+      label: {
+        text: props.name,
+        className: "markerLabel",
+        color: "#ffffff",
+        fontSize: "13px",
+      },
       icon: {
-        url: image,
-        scaledSize: new google.maps.Size(100, 100),
+        url: "img/marker-small.png",
       },
     });
 
     marker.addListener("click", function () {
       map.setZoom(17);
       map.setCenter(latLng);
+
+      document.getElementById("side").classList.add("active");
+      var img = document.getElementById("side-image");
+      img.style.objectFit = "cover";
+      img.src = props.image;
       document.getElementById("side-title").innerHTML = props.name;
+      document.getElementById("side-desc-head").innerHTML = props.header;
       document.getElementById("side-desc").innerHTML = props.description;
     });
 
     marker.addListener("mouseover", function () {
+      marker.setLabel({
+        text: props.name + props.header,
+        className: "markerLabelClick",
+        color: "#ffffff",
+        fontSize: "20px",
+      });
+
       marker.setIcon({
-        url: image,
-        scaledSize: new google.maps.Size(200, 200),
+        url: "img/marker.png",
       });
     });
 
     marker.addListener("mouseout", function () {
+      marker.setLabel({
+        text: props.name,
+        className: "markerLabel",
+        color: "#ffffff",
+        fontSize: "13px",
+      });
+
       marker.setIcon({
-        url: image,
-        scaledSize: new google.maps.Size(100, 100),
+        url: "img/marker-small.png",
       });
     });
-
-    //   const markerLabel = new google.maps.MarkerLabel({
-    //     text: props.name,
-    //   });
-
-    // Check Content
-    // if (props.name) {
-    //   // The InfoWindow
-    //   //   const markerLabel = new google.maps.MarkerLabel({
-    //   //     text: props.name,
-    //   //   });
-    //   marker.addListener("click", function () {
-    //     map.setZoom(17);
-    //     map.setCenter(latLng);
-
-    //   });
-
-    // }
   }
 }
 
 window.initMap = initMap;
+
+function removeSide() {
+  document.getElementById("side").classList.remove("active");
+}
